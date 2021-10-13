57e2120b21fa60a524006dab52fcfe4f4578d322
Roman Leshchuk <leshchukroman@gmail.com>
2021-10-13 19:07:36 +0300

    refactoring code with OOP method without comunication of button but with all elements which must be on page

--- 10 Classes Object oriented Programming (OOP)/01 OOP starting setup/assets/scripts/app.js
+++ 10 Classes Object oriented Programming (OOP)/01 OOP starting setup/assets/scripts/app.js
@@ -1,40 +1,104 @@
-const productList = {
-  products: [
-    {
-      title: "A Pillow",
-      imageUrl: "https://m.media-amazon.com/images/I/51DOorpA5UL._AC_SX466_.jpg",
-      price: 19.99,
-      description: "A soft pillows"
-    },
-    {
-      title: "A Carpet",
-      imageUrl: "https://images-na.ssl-images-amazon.com/images/G/01/img17/furniture/Rug_Style_Oriental_440x300_hor.jpg",
-      price: 89.99,
-      description: "A carpet wuch you migth like - or not."
-    }
-  ],
+class Product {
+  title = "DEFAULT";
+  imageUrl;
+  description;
+  price;
+
+  constructor(title, image, desc, price) {
+    this.title = title;
+    this.imageUrl = image;
+    this.description = desc;
+    this.price = price;
+  }
+}
+
+class ShopingCart {
+  items = [];
+
   render() {
-    const renderHook = document.getElementById('app');
-    const prodList = document.createElement('ul');
-    prodList.className = 'product-list';
-    for (const prod of this.products) {
-      const prodEl = document.createElement('li');
-      prodEl.className = 'product-item';
-      prodEl.innerHTML = `
-        <div>
-          <img src="${prod.imageUrl}" alt="${prod.title}">
-          <div class="product-item__content">
-            <h2>${prod.title}</h2>
-            <h3>\$${prod.price}</h3>
-            <p>${prod.description}</p>
-            <button>Add to Cart</button>
-          </div>
+    const cartEl = document.createElement('section');
+    cartEl.innerHTML = `
+      <h2>Total: \$${0}</h2>
+      <button>Order Now!</button>
+    `;
+    cartEl.className = 'cart';
+    return cartEl;
+  }
+}
+
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  addToCart() {
+    console.log('Adding product to cart...');
+    console.log(this.product);
+  }
+
+  render() {
+    const prodEl = document.createElement("li");
+    prodEl.className = "product-item";
+    prodEl.innerHTML = `
+      <div>
+        <img src="${this.product.imageUrl}" alt="${this.product.title}">
+        <div class="product-item__content">
+          <h2>${this.product.title}</h2>
+          <h3>\$${this.product.price}</h3>
+          <p>${this.product.description}</p>
+          <button>Add to Cart</button>
         </div>
-      `;
+      </div>
+    `;
+    const addCartButton = prodEl.querySelector('button');
+    addCartButton.addEventListener('click', this.addToCart.bind(this));
+    return prodEl;
+  }
+}
+
+class ProductList {
+  products = [
+    new Product(
+      "A Pillow",
+      "https://m.media-amazon.com/images/I/51DOorpA5UL._AC_SX466_.jpg",
+      "A soft pillows",
+      19.99
+    ),
+    new Product(
+      "A Carpet",
+      "https://images-na.ssl-images-amazon.com/images/G/01/img17/furniture/Rug_Style_Oriental_440x300_hor.jpg",
+      "A carpet wuch you migth like - or not.",
+      89.99
+    ),
+  ];
+
+  constructor() {}
+
+  render() {
+    const prodList = document.createElement("ul");
+    prodList.className = "product-list";
+    for (const prod of this.products) {
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
       prodList.append(prodEl);
     }
-    renderHook.append(prodList);
+    return prodList;
   }
-};
+}
 
-productList.render();
+class Shop {
+  render() {
+    const renderHook = document.getElementById("app");
+
+    const cart = new ShopingCart();
+    const cartEl = cart.render();
+    const productList = new ProductList();
+    const prodListEl = productList.render();
+
+    renderHook.append(cartEl);
+    renderHook.append(prodListEl);
+  }
+}
+
+const shop = new Shop();
+shop.render();
