94e5090349539e859c7827101e902ed5eefe4c44
Roman Leshchuk <leshchukroman@gmail.com>
2021-10-29 18:08:20 +0300

    libraries general information and how can be implemented on the project lodash, axios, jquery was mentioned

---
+++ 19 Working with JavaScript Libraries/01 starting setup/app.js
@@ -0,0 +1,13 @@
+Math.random();
+
+function random() {
+
+}
+
+const customers = ['Max', 'Manuel', 'Anna'];
+
+const activeCustomers = ['Max', 'Manuel'];
+
+const inactiveCustomers = _.difference(customers, activeCustomers);
+
+console.log(inactiveCustomers);
