effb3a714c1bd6ec958f4344e43f5934ccb0c01f
Roman Leshchuk <leshchukroman@gmail.com>
2021-11-09 21:09:07 +0200

    remove API key

--- 24 Time to Practice Share My Place App/01 practice starting setup/src/Utility/Location.js
+++ 24 Time to Practice Share My Place App/01 practice starting setup/src/Utility/Location.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = 'AIzaSyCicx4XGqL1aZ4m_kbMoil0VYInlcZgVjQ';
+const GOOGLE_API_KEY = 'put ur Api key here';
 
 export async function getAddressFromCoords(coords) {
   const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${coords.lat},${coords.lng}&key=${GOOGLE_API_KEY}`);
