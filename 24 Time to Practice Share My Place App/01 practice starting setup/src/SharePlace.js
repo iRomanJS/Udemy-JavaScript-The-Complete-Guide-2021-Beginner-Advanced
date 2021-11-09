7ec33fe22dd04100d68cfe1fe28ff7af07ba5625
Roman Leshchuk <leshchukroman@gmail.com>
2021-11-09 20:46:52 +0200

    website made with obj oriented programming webpack helps to make all js file in one for fast working expirience google maps API helps to cover real information main story was to make fetch request and cover information to fields also copied clipboard function was made to cover this kinde of methods to practice

---
+++ 24 Time to Practice Share My Place App/01 practice starting setup/src/SharePlace.js
@@ -0,0 +1,99 @@
+import { Modal } from "./UI/Modal";
+import { Map } from "./UI/Map";
+import { getCoordsFromAddress, getAddressFromCoords } from './Utility/Location';
+
+class PlaceFinder {
+  constructor() {
+    const addressForm = document.querySelector("form");
+    const locateUserBtn = document.getElementById("locate-btn");
+    this.shareBtn = document.getElementById('share-btn');
+
+    locateUserBtn.addEventListener("click", this.locateUserHandler.bind(this));
+    addressForm.addEventListener("submit", this.findAddressHandler.bind(this));
+    this.shareBtn.addEventListener('click', this.sharePlaceHandler);
+
+  }
+
+  sharePlaceHandler() {
+    const sharedLinkInputElement = document.getElementById('share-link');
+    sharedLinkInputElement.select();
+    if (!navigator.clipboard) {
+      return;
+    }
+
+    navigator.clipboard
+      .writeText(sharedLinkInputElement.value)
+      .then(() => {
+        alert('Copied into clipboard!');
+      })
+      .catch((err) => {
+        console.log(err);
+        sharedLinkInputElement.select();
+      });
+  }
+
+  selectPlace(coordinates, address) {
+    if (this.map) {
+      this.map.render(coordinates);
+    } else {
+      this.map = new Map(coordinates);
+    }
+    this.shareBtn.disabled = false;
+    const sharedLinkInputElement = document.getElementById('share-link');
+    sharedLinkInputElement.value = `${location.origin}/my-place?address=${encodeURI(address)}&lat=${coordinates.lat},&lng=${coordinates.lng}`;
+  }
+
+  locateUserHandler() {
+    if (!navigator.geolocation) {
+      alert(
+        "Location feature is not available in your browser - please ise a more modern browser or manually enter an address."
+      );
+      return;
+    }
+    const modal = new Modal(
+      "loading-modal-content",
+      "Loading location - please wait!"
+    );
+    modal.show();
+    navigator.geolocation.getCurrentPosition(
+      async (succesResult) => {
+        const coordinates = {
+          lat: succesResult.coords.latitude,
+          lng: succesResult.coords.longitude,
+        };
+        const address = await getAddressFromCoords(coordinates);
+        modal.hide();
+        this.selectPlace(coordinates, address);
+      },
+      (error) => {
+        modal.hide();
+        alert(
+          "Could not locate you unfortunately. Please enter an address manually!"
+        );
+      }
+    );
+  }
+
+  async findAddressHandler(event) {
+    event.preventDefault();
+    const address = event.target.querySelector("input").value;
+    if (!address || address.trim().length === 0) {
+      alert("Invalid address entered - please try again!");
+      return;
+    }
+    const modal = new Modal(
+      "loading-modal-content",
+      "Loading location - please wait!"
+    );
+    modal.show();
+    try {
+      const coordinates = await getCoordsFromAddress(address);
+      this.selectPlace(coordinates, address);
+    } catch (err) {
+      alert(err.message);
+    }
+    modal.hide();
+  }
+}
+
+const placeFinder = new PlaceFinder();
